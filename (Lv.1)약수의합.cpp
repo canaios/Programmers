d0a82fa2d531fb7fd474fbb078c2a5cca7c3083e
cana <94155642+canaios@users.noreply.github.com>
2024-02-16 10:29:52 +0900

    Add files via upload

---
+++ (Lv.1)약수의합.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int sum = 0;
+
+    if (n == 0) {
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
